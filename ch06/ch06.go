4748cc23f986c3f26220dc1e79a4adf22fa92467
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2023-09-23 20:58:38 -0500

    got to deleting

--- ch06/ch06.go
+++ ch06/ch06.go
@@ -1,19 +1,73 @@
 package ch06
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-type TrieNode struct {
-	IsEntry  bool
-	Children []*TrieNode
+type Node struct {
+	Char     string
+	Children [26]*Node
+}
+
+func NewNode(char string) *Node {
+	node := &Node{Char: char}
+	for i := 0; i < 26; i++ {
+		node.Children[i] = nil
+	}
+	return node
 }
 
 type Trie struct {
-	root *TrieNode
+	RootNode *Node
+}
+
+func NewTrie() *Trie {
+	root := NewNode("\000")
+	return &Trie{RootNode: root}
+}
+
+func (t *Trie) Insert(word string) error {
+	current := t.RootNode
+	strippedWord := strings.ToLower(strings.ReplaceAll(word, " ", ""))
+	for i := 0; i < len(strippedWord); i++ {
+		index := strippedWord[i] - 'a'
+		if current.Children[index] == nil {
+			current.Children[index] = NewNode(string(strippedWord[i]))
+		}
+		current = current.Children[index]
+	}
+	return nil
+}
+
+func (t *Trie) Search(word string) bool {
+	strippedWord := strings.ToLower(strings.ReplaceAll(word, " ", ""))
+	current := t.RootNode
+	for i := 0; i < len(strippedWord); i++ {
+		index := strippedWord[i] - 'a'
+		if current == nil || current.Children[index] == nil {
+			return false
+		}
+		current = current.Children[index]
+	}
+	return true
 }
 
 func Run() {
 	fmt.Println("----------------ch06 start-----------------")
+	tr := NewTrie()
 
-	fmt.Println("hello world")
+	words := []string{"ben", "steven", "sardyboy", "samantha", "sardy", "bob", "sam"}
+	allWords := []string{"not  ", "in ", "the", "trie"}
+
+	for _, word := range words {
+		tr.Insert(word)
+		allWords = append(allWords, word)
+	}
+
+	for _, word := range allWords {
+		fmt.Printf("search result for %s: %t\n", word, tr.Search(word))
+	}
+
 	fmt.Println("----------------ch06 end-----------------")
 }
--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"github.com/bahodge/data_structures_the_fun_way/ch01"
-	"github.com/bahodge/data_structures_the_fun_way/ch02"
-	"github.com/bahodge/data_structures_the_fun_way/ch03"
-	"github.com/bahodge/data_structures_the_fun_way/ch04"
-	"github.com/bahodge/data_structures_the_fun_way/ch05"
+	// "github.com/bahodge/data_structures_the_fun_way/ch01"
+	// "github.com/bahodge/data_structures_the_fun_way/ch02"
+	// "github.com/bahodge/data_structures_the_fun_way/ch03"
+	// "github.com/bahodge/data_structures_the_fun_way/ch04"
+	// "github.com/bahodge/data_structures_the_fun_way/ch05"
 	"github.com/bahodge/data_structures_the_fun_way/ch06"
 )
 
 func main() {
-	ch01.Run()
-	ch02.Run()
-	ch03.Run()
-	ch04.Run()
-	ch05.Run()
+	// ch01.Run()
+	// ch02.Run()
+	// ch03.Run()
+	// ch04.Run()
+	// ch05.Run()
 	ch06.Run()
 }
